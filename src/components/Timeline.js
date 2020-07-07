fe7bf1dea9ffe099c75e898efe064f7c648e7847
Brett Schellenberg <brett.schellenberg@gmail.com>
2020-07-07 09:22:19 -0600

    clean up util

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,27 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import dayjs from 'dayjs';
-
+import { parseMillisecondsIntoReadableTime }from '../util/time';
 const msPerPixel = 5000; // 1000 means 1 pixel per second
 
-// takes a number of milliseconds and returns a human readable duration
-function parseMillisecondsIntoReadableTime(milliseconds){
-    //Get hours from milliseconds
-    var hours = milliseconds / (1000*60*60);
-    var absoluteHours = Math.floor(hours);
-    var h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
-  
-    //Get remainder from hours and convert to minutes
-    var minutes = (hours - absoluteHours) * 60;
-    var absoluteMinutes = Math.floor(minutes);
-    var m = absoluteMinutes > 9 ? absoluteMinutes : '0' +  absoluteMinutes;
-  
-    //Get remainder from minutes and convert to seconds
-    var seconds = (minutes - absoluteMinutes) * 60;
-    var absoluteSeconds = Math.floor(seconds);
-    var s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds;
-  
-    return h + ':' + m + ':' + s;
-  }
   
 
 export default (props) => {
